ee42e176775dd6fe927fd518109521639c8e4de7
rs667119 <ranjani.shivashankar@broadcom.com>
2020-07-03 13:40:05 +0530

    Added '@' so that users don't have to enter that while searching

--- simple-vue-app/src/components/layout/Header.vue
+++ simple-vue-app/src/components/layout/Header.vue
@@ -8,6 +8,7 @@
 			<!-- Right aligned nav items -->
 			<b-navbar-nav class="ml-auto">
 				<b-nav-form>
+					<b-button size="sm" variant="warning" class="my-2 my-sm-0">@</b-button>
 					<b-form-input id="search-user" size="sm" :state="search"  class="mr-sm-2" placeholder="Search"></b-form-input>
 					<b-button size="sm" variant="dark" class="my-2 my-sm-0" type="button" @click="$emit('submit-search')">Search</b-button>
 				</b-nav-form>
